d18bfcdad94ee22816350560ae0ca96b5c93169a
Osiris <potterosiris@gmail.com>
2020-10-20 20:35:34 -0400

    route controllers

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
+const placesControllers = require("../controllers/places-controllers");
+
 const router = express.Router();
 
-router.get("/:pid");
+router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/user/:uid");
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
 module.exports = router;
