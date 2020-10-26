8513509252db5189a3a745a84b1570b61156f07a
Osiris <potterosiris@gmail.com>
2020-10-25 20:28:56 -0400

    app.js updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const journalsRoutes = require("./routes/journals-routes");
 const placesRoutes = require("./routes/places-routes");
 const HttpError = require("./models/http-error");
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes);
+
+app.use("/api/journal");
 
 app.use((req, res, next) => {
   const error = new HttpError("could not find this route", 404);
