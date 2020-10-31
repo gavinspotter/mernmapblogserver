7e96443ca7ba0892cfeca04fdd135e24374cedf1
Osiris <potterosiris@gmail.com>
2020-10-31 14:05:36 -0400

    try catch blog get place by id

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -20,15 +20,27 @@ let DUMMY_PLACES = [
   },
 ];
 
-const getPlaceById = (req, res, next) => {
+const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
 
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
+  let place;
+
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      "something went wrong, could not find a place",
+      500
+    );
+    return next(error);
+  }
 
   if (!place) {
-    throw new HttpError("could not find a place for the provided id.", 404);
+    const error = new HttpError(
+      "could not find a place for the provided id.",
+      404
+    );
+    return next(error);
   }
 
   res.json({ place: place });
