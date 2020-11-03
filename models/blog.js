792da840bc6adb84cff24606c7139ebcc6215323
Osiris <potterosiris@gmail.com>
2020-11-03 12:46:27 -0500

    blog js schema

---
+++ models/blog.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+  blgentry: { type: String, required: true },
+  creator: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
