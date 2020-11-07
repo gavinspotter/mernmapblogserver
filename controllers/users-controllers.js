7499be4a2f7b3396aacbfd578d3a6d660f7f7850
Osiris <potterosiris@gmail.com>
2020-11-07 14:26:35 -0500

    created user

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -46,16 +46,22 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-  const createdUser = {
-    id: uuidv4(),
+  const createdUser = new User({
     name,
     email,
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/d6/Ra_Barque.jpg",
     password,
-  };
+    places,
+  });
 
-  DUMMY_USER.push(createdUser);
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError("signing up failed please try again.", 500);
+    return next(error);
+  }
 
-  res.status(201).json({ user: createdUser });
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
 const login = (req, res, next) => {
