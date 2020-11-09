99587987aea6cf0837b15499960294557d37a000
Osiris <potterosiris@gmail.com>
2020-11-09 13:16:22 -0500

    journals added to schema

--- models/user.js
+++ models/user.js
@@ -9,6 +9,7 @@ const userSchema = new Schema({
   password: { type: String, required: true, minlength: 6 },
   image: { type: String, required: true },
   places: [{ type: mongoose.Types.ObjectId, required: true, ref: "Place" }],
+  journals: [{ type: mongoose.Types.ObjectId, required: true, ref: "Journal" }],
 });
 
 userSchema.plugin(uniqueValidator);
