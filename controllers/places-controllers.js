f6c5f90bab0ad73b3f4e9ad77752ed5d25d4a3ef
Osiris <potterosiris@gmail.com>
2020-12-07 18:27:19 -0500

    fs unlink

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -191,6 +191,9 @@ const deletePlace = async (req, res, next) => {
     return next(error);
   }
 
+  fs.unlink(imagePath, err => {
+    console.log(err)
+  })
 
 
   res.status(200).json({ message: "deleted place" });
