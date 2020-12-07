b01d4ec8af66770fa1b2a6aab854c91fec7adc72
Osiris <potterosiris@gmail.com>
2020-12-07 16:58:54 -0500

    mime type map

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,5 +1,10 @@
 const multer = require("multer")
 
+const MIMI_TYPE_MAP = {
+    "image/png": "png",
+    "image/jpeg": "jpeg",
+    "image/jpg": "jpg",
+}
 const fileUpload = multer({
     limits: 5000000,
     storage: multer.diskStorage({
