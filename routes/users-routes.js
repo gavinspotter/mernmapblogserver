9036507d7146907380c68253530d018dca230434
Osiris <potterosiris@gmail.com>
2020-12-07 16:53:11 -0500

    require multer

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -4,6 +4,8 @@ const { check } = require("express-validator");
 
 const usersController = require("../controllers/users-controllers");
 
+const fileUpload = require("../middleware/file-upload")
+
 const router = express.Router();
 
 router.get("/", usersController.getUsers);
