01b07e072b277592eb5a3129e21a76591bd1da08
Osiris <potterosiris@gmail.com>
2020-12-07 18:03:31 -0500

    require path

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const fs = require("fs")
+const path = require("path")
+
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -14,7 +16,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/uploads/images")
+app.use("/uploads/images", express.static())
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
