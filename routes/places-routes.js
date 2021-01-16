8f265697771fc2a0fb7e9cdb252cf51ec1d581d4
Osiris <potterosiris@gmail.com>
2021-01-16 15:48:20 -0500

    check auth

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -4,6 +4,7 @@ const { check } = require("express-validator");
 
 const placesControllers = require("../controllers/places-controllers");
 const fileUpload = require("../middleware/file-upload")
+const checkAuth = require("../middleware/check-auth")
 
 const router = express.Router();
 
@@ -11,7 +12,7 @@ router.get("/:pid", placesControllers.getPlaceById);
 
 router.get("/user/:uid", placesControllers.getPlacesByUserId);
 
-router.use()
+router.use(checkAuth)
 
 router.post(
   "/",
