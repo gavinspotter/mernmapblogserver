893dd876a831d62980a228e04c1f92901a10b01b
Osiris <potterosiris@gmail.com>
2021-01-16 15:43:48 -0500

    tc

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,9 +1,16 @@
 const HttpError = require("../models/http-error");
 
 module.exports = (req, res, next) => {
-    const token = req.headers.authorization.split(" ")[1];
-    if (!token) {
-        const error = new HttpError('Authentication failed', 401)
+
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        if (!token) {
+            const error = new HttpError('Authentication failed', 401)
+            return next(error)
+        }
+    } catch (err) {
+        const error = new HttpError("authorization failed", 401)
         return next(error)
     }
+
 }
